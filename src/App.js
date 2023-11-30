dde7c307acc40585f1ce86ac96c68f3e08d6c7b9
FernandaSGomes <nandasthefany@hotmail.com>
2023-11-30 18:14:11 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Fernanda Gomes</h1>
-      <h2>Front-end Developer</h2>
-      <footer>
-        This project was coded by
-        <a
-          href="www.linkedin.com/in/fernanda-gomes-front-end-developer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Fernanda Gomes
-        </a>{" "}
-        and is open-source on{" "}
-        <a
-          href="https://github.com/FernandaSGomes/fernanda-gomes-portfolio"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Fernanda Gomes</h1>
+        <h2>Front-end Developer</h2>
+        <footer>
+          This project was coded by
+          <a
+            href="www.linkedin.com/in/fernanda-gomes-front-end-developer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Fernanda Gomes
+          </a>{" "}
+          and is open-source on{" "}
+          <a
+            href="https://github.com/FernandaSGomes/fernanda-gomes-portfolio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
