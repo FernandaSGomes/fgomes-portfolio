9cee98262159bf6ea47a3ef73ef0a1890d1d2c19
FernandaSGomes <nandasthefany@hotmail.com>
2023-12-05 03:44:28 +0000

    Added projects images

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,10 @@
 import React from "react";
-import fgomes from "./images/fgomespicture.png";
+import gibraltar from "./images/gibraltar.png";
+import portugal from "./images/portugal.png";
+import clock from "./images/clock.png";
 import Nav from "./Nav";
 import Footer from "./Footer";
+
 import "./App.css";
 
 export default function Projects() {
@@ -11,12 +14,29 @@ export default function Projects() {
         <Nav />
 
         <h1>Projects</h1>
-
-        <img
-          src={fgomes}
-          alt="fgomes"
-          className="picture d-none d-lg-block img-fluid rounded mb-2"
-        />
+        <div className="row">
+          <div className="col-sm-4 mb-3">
+            <img
+              src={gibraltar}
+              alt="Gibraltar project"
+              className="gibraltar d-none d-lg-block img-fluid rounded mb-2 p-2 border shadow-sm"
+            />
+          </div>
+          <div className="col-sm-4 mb-3">
+            <img
+              src={portugal}
+              alt="Portugal travel project"
+              className="portugal d-none d-lg-block img-fluid rounded mb-2 p-2 border shadow-sm"
+            />
+          </div>
+          <div className="col-sm-4 mb-3">
+            <img
+              src={clock}
+              alt="World-clock project"
+              className="clock d-none d-lg-block img-fluid rounded mb-2 p-2 border shadow-sm"
+            />
+          </div>
+        </div>
         <Footer />
       </div>
     </div>
